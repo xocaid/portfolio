32d034b1205c2e5c426c89c1c779c6b25b801e9e
Xochitl <xocaid@gmail.com>
2024-03-05 12:01:50 -0800

    icons added to footer

--- pages/margins/footer.tsx
+++ pages/margins/footer.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
     return (
         <div className="footer">
             <div>
+                <a href='https://www.linkedin.com/in/xochitlca/' title='LinkedIn_Icon'>
+                    <FaLinkedin />
+                </a>
+                <a href='https://github.com/xocaid' title="GitHub_Icon">
+                    <FaGithub />
+                </a>
                 <p>Copyright &copy; 2024 Portfolio. All Right Reserved.</p>
             </div>
         </div>
--- pages/margins/header.tsx
+++ pages/margins/header.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import About from "../sections/about";
+import Projects from "../sections/projects";
+import Contact from "../sections/contact";
 
 const Header = () => {
     return (
         <div className="header">
 
             <p>Header</p>
+            <div>
+                {/* Tabs for Header */}
+            </div>
         </div>
     )
 }
